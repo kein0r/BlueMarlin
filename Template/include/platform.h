31745ccb9bda7043f5509fd04f68516ccb0e908b
kein0r <github@dummgebabbel.de>
2016-12-02 11:29:05 +0800

    Template files and folders added

---
+++ Template/include/platform.h
@@ -0,0 +1,36 @@
+#if (!defined TEMPLATE_INCLUDE_TEMPLATE_H_)
+/* Preprocessor exclusion definition */
+#define TEMPLATE_INCLUDE_TEMPLATE_H_
+/**
+ * \file Template.h
+ *
+ * \brief Template include file
+ *
+ * Include files should start with a lowercase character and use cammelCase
+ * notation.
+ *
+ * \project BlueMarlin
+ * \author <FULL NAME>
+ *
+ */
+
+
+/** \addtogroup Template
+ * @{
+ */
+
+/* ******************| Inclusions |************************************ */
+
+/* ******************| Macros |**************************************** */
+
+/* ******************| Type definitions |****************************** */
+
+/* ******************| External function declarations |**************** */
+
+/* ******************| External constants |**************************** */
+
+/* ******************| External variables |**************************** */
+
+/** @} doxygen end group definition */
+#endif /* if !defined( TEMPLATE_INCLUDE_TEMPLATE_H_ ) */
+/* ******************| End of file |*********************************** */
---
+++ Template/src/template.c
@@ -0,0 +1,50 @@
+/**
+ * \file template.c
+ *
+ * \brief Template source file
+ *
+ * File names should start with lowercase character and use cammelCase 
+ * notation.
+ *
+ * \project BlueMarlin
+ * \author <FULL NAME>
+ *
+ */
+
+
+/** \addtogroup Template
+ * @{
+ */
+
+/* ******************| Inclusions |************************************ */
+#include "template.h"
+
+/* ******************| Macros |**************************************** */
+
+/* ******************| Type Definitions |****************************** */
+
+/* ******************| Function Prototypes |*************************** */
+int Template_templateFunction(int i, int *s);
+
+
+/* ******************| Global Variables |****************************** */
+
+/* ******************| Function Implementation |*********************** */
+/**
+ * \brief Template function
+ * Document your function here. Function names shall start with module names
+ * followed by an underscore followed by the function name starting with a 
+ * lowercase letter using cammelCase notion.
+ * Dont' forget to also describe the parameters of your function including
+ * their direction. Also describe the return values, if any.
+ * @param[in]  		i	Example input parameter
+ * @param[in/out]	i	Example input/output parameter
+ * @return			This function returns result X
+*/
+int Template_templateFunction(int i, int *s)
+{
+	return i;
+}
+
+/** @} doxygen end group definition */
+/* ******************| End of file |*********************************** */
