ca65b1d98c1920a61e934866d88c6210c082e93e
kein0r <github@dummgebabbel.de>
2016-12-02 11:57:36 +0800

    Adding platform module based on template

---
+++ Platform_WindowsX86/test/Template_test_drv2.c
@@ -0,0 +1,50 @@
+/**
+ * \file template_test_drv.c
+ *
+ * \brief Template source file
+ *
+ * File names should start with lowercase character and use cammelCase
+ * notation.
+ *
+ * \project BlueMarlin
+ * \author <FULL NAME>
+ *
+ */
+
+
+/** \addtogroup Template
+ * @{
+ */
+
+/* ******************| Inclusions |************************************ */
+
+/* ******************| Macros |**************************************** */
+
+/* ******************| Type Definitions |****************************** */
+
+/* ******************| Function Prototypes |*************************** */
+int TemplateTest_templateFunction(int i, int *s);
+
+
+/* ******************| Global Variables |****************************** */
+
+/* ******************| Function Implementation |*********************** */
+/**
+ * \brief Template function
+ * Document your test function here. Test function names shall start with 
+ * module name immediately followed by Test followed by an underscore 
+ * followed by the function name starting with a lowercase letter using 
+ * cammelCase notion.
+ * Don't forget to also describe the parameters of your function including
+ * their direction. Also describe the return values, if any.
+ * @param[in]  		i	Example input parameter
+ * @param[in/out]	i	Example input/output parameter
+ * @return			This function returns result X
+*/
+int TemplateTest_templateFunction(int i, int *s)
+{
+	return i;
+}
+
+/** @} doxygen end group definition */
+/* ******************| End of file |*********************************** */
---
+++ Platform_WindowsX86/test/Template_test_drv2.h
@@ -0,0 +1,33 @@
+#if (!defined TEMPLATE_TEST_DRV_H_)
+/* Preprocessor exclusion definition */
+#define TEMPLATE_TEST_DRV_H_
+/** 
+ * \file template_test_drv.h 
+ * 
+ * \brief Template include file for test driver
+ *
+ * \project BlueMarlin
+ * \author <FULL NAME>
+ *
+ */
+
+
+/** \addtogroup Template_unittest
+ * @{
+ */
+
+/* ******************| Inclusions |************************************ */
+
+/* ******************| Macros |**************************************** */
+
+/* ******************| Type definitions |****************************** */
+
+/* ******************| External function declarations |**************** */
+
+/* ******************| External constants |**************************** */
+
+/* ******************| External variables |**************************** */
+
+/** @} doxygen end group definition */
+#endif /* if !defined( TEMPLATE_TEST_DRV_H_ ) */
+/* ******************| End of file |*********************************** */
