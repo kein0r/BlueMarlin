af93d36ca37a92c8b0aa573729484c95426fdc13
kein0r <github@dummgebabbel.de>
2016-12-04 19:37:29 +0800

    Added GPL header to all files (copy from Marlin)

--- Template/include/template.h
+++ Template/include/template.h
@@ -1,3 +1,24 @@
+/**
+ * BlueMarlin 3D Printer Firmware
+ * Copyright (C) 2016 BlueMarlinFirmware [https://github.com/kein0r/BlueMarlin]
+ *
+ * Based on Marlin, Sprinter and grbl.
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 #if (!defined TEMPLATE_INCLUDE_TEMPLATE_H_)
 /* Preprocessor exclusion definition */
 #define TEMPLATE_INCLUDE_TEMPLATE_H_
