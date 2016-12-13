38d5c7fcea9f5666fda2033a42cb0cb16d6272d8
kein0r <kein0r@dummgebabbel.de>
2016-12-13 21:20:10 +0800

    Added new GCodeRingBuffer from template

---
+++ GCodeRingBuffer/test/Template_test_drv_stub.c
@@ -0,0 +1,53 @@
+/**
+ * \file template_test_drv_stub.c
+ *
+ * \brief Template source file for unit test stubs
+ *
+ * All stubs needed for the unit test of this particular modules shall
+ * be done within this file.
+ *
+ * File names should start with lowercase character and use cammelCase
+ * notation.
+ *
+ * \project BlueMarlin
+ * \author <FULL NAME>
+ *
+ */
+
+
+/** \addtogroup Template
+ * @{
+ */
+
+/* ******************| Inclusions |************************************ */
+
+/* ******************| Macros |**************************************** */
+
+/* ******************| Type Definitions |****************************** */
+
+/* ******************| Function Prototypes |*************************** */
+int TemplateTest_templateStub(int i, int *s);
+
+
+/* ******************| Global Variables |****************************** */
+
+/* ******************| Function Implementation |*********************** */
+/**
+ * \brief Template function
+ * Document your test function here. Test function names shall start with 
+ * module name immediately followed by Test followed by an underscore 
+ * followed by the function name starting with a lowercase letter using 
+ * cammelCase notion.
+ * Don't forget to also describe the parameters of your function including
+ * their direction. Also describe the return values, if any.
+ * @param[in]  		i	Example input parameter
+ * @param[in/out]	i	Example input/output parameter
+ * @return			This function returns result X
+*/
+int TemplateTest_templateStub(int i, int *s)
+{
+	return i;
+}
+
+/** @} doxygen end group definition */
+/* ******************| End of file |*********************************** */
